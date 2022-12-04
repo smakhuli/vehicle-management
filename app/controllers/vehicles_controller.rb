f797f1746a1680ca5db72f3c145146df9ae243df
Sami Makhuli <roger.makhuli@gmail.com>
2022-12-04 16:22:14 -0600

    Added vehicle model validations, preliminary new and show pages and controller logic

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,9 +3,34 @@ class VehiclesController < ApplicationController
     @vehicles = Vehicle.all
   end
 
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    # raise params.inspect
+    @vehicle = Vehicle.new(vehicle_params)
+
+    if @vehicle.save
+      redirect_to @vehicle
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
   def destroy
     @vehicle = Vehicle.find(params[:id])
     @vehicle.destroy
     redirect_to vehicles_path
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:vin, :year, :make, :model, :trim)
   end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,6 @@
 class Vehicle < ApplicationRecord
+  validates :vin, presence: true
+  validates :year, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
 end
